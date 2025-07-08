3cf1b0ef1c1f84469fbf2021382c37a2edd17c62
YassoKhalid <153525555+YassoKhalid@users.noreply.github.com>
2025-07-09 00:41:21 +0300

    Updated the DemoBook Trial

--- BookStore/src/Main.java
+++ BookStore/src/Main.java
@@ -4,8 +4,8 @@ import java.time.Month;
 public class Main {
     public static void main(String[] args) {
         BookStore store = new BookStore();
-        DemoBook ebook=new DemoBook("E1", "The blue elephant", LocalDate.of(2010, Month.AUGUST, 23), 50);
-        store.AddBook(ebook, 2);
+        DemoBook demobook = new DemoBook("E1", "The blue elephant", LocalDate.of(2010, Month.AUGUST, 23), 50);
+        store.AddBook(demobook, 2);
         //trying to Try reading Demobook
         store.TryDemo("E1");
     }
