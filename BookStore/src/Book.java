718f159402252331c1c53f9e157390e2bb0fa0e9
YassoKhalid <153525555+YassoKhalid@users.noreply.github.com>
2025-07-09 00:36:10 +0300

    Updated the DemoBook Trial

--- BookStore/src/Book.java
+++ BookStore/src/Book.java
@@ -15,6 +15,13 @@ public class Book {
         this.deliveryService = deliveryService;
     }
 
+    Book(String ISBN, String Title, LocalDate PublicationDate, double Price) {
+        this.ISBN = ISBN;
+        this.Title = Title;
+        this.PublicationDate = PublicationDate;
+        this.Price = Price;
+    }
+
     String getTitle() {
         return Title;
     }
@@ -31,7 +38,7 @@ public class Book {
         return ISBN;
     }
 
-    public void Deliver(int quantity,String Email,  String DestinationAddress) {
+    public void Deliver(int quantity, String Email, String DestinationAddress) {
         deliveryService.Deliver(this, quantity, DestinationAddress);
     }
 
--- BookStore/src/DemoBook.java
+++ BookStore/src/DemoBook.java
@@ -1,7 +1,7 @@
 import java.time.LocalDate;
 
 public class DemoBook extends Book {
-    public DemoBook(String ISBN, String Title, LocalDate PublicationDate, double Price, DeliveryService deliveryService) {
-        super(ISBN, Title, PublicationDate, Price, deliveryService);
+    public DemoBook(String ISBN, String Title, LocalDate PublicationDate, double Price) {
+        super(ISBN, Title, PublicationDate, Price);
     }
 }
